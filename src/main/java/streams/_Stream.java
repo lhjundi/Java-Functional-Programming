912d317ba9f2664a92b208da8ecc5b1e82e04d7e
Lucas Jundi Hikazudani <lhjundi@outlook.com>
2024-05-01 22:20:02 -0300

    Streams

---
+++ src/main/java/streams/_Stream.java
@@ -0,0 +1,77 @@
+package streams;
+
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.IntConsumer;
+import java.util.function.Predicate;
+import java.util.function.ToIntFunction;
+import java.util.stream.Collectors;
+
+import static streams._Stream.Gender.*;
+
+public class _Stream {
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+                new Person("John", MALE),
+                new Person("Maria", FEMALE),
+                new Person("Maria", FEMALE),
+                new Person("Aisha", FEMALE),
+                new Person("Alex", MALE),
+                new Person("Alice", FEMALE),
+                new Person("Joshua", PREFER_NOT_TO_SAY)
+        );
+
+        people.stream()
+                .map(person -> person.gender)
+                .collect(Collectors.toSet())
+                .forEach(System.out::println);
+
+        people.stream()
+                .map(person -> person.name)
+                .collect(Collectors.toSet())
+                .forEach(System.out::println);
+
+        Function<Person, String> personStringFunction = person -> person.name;
+        ToIntFunction<String> length = String::length;
+        IntConsumer println = System.out::println;
+        people.stream()
+                .map(personStringFunction)
+                .mapToInt(length)
+                .forEach(println);
+
+        Predicate<Person> personPredicate = person -> FEMALE.equals(person.gender);
+        boolean containsOnlyFemales = people.stream()
+                .allMatch(personPredicate);
+        System.out.println(containsOnlyFemales);
+
+        boolean containsFemales = people.stream()
+                .anyMatch(personPredicate);
+        System.out.println(containsFemales);
+
+        boolean noOnePreferNotToSayGender = people.stream()
+                .noneMatch(person -> PREFER_NOT_TO_SAY.equals(person.gender));
+        System.out.println(noOnePreferNotToSayGender);
+    }
+    static class Person{
+        private final String name;
+        private final Gender gender;
+
+        public Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+    enum Gender{
+        MALE, FEMALE, PREFER_NOT_TO_SAY
+    }
+
+}
